fbb96660302850479a7929963d9e29a5c7606643
adelina gutu <w1705972@gmail.com>
2025-01-28 00:45:07 +0000

    book , club and meeting controllers and index pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+
+  resources :clubs, only: [:index]
+  resources :books, only: [:index]
+  resources :meetings, only: [:index]
+  
   # Defines the root path route ("/")
   # root "posts#index"
 end
