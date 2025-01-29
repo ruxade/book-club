997507466b59c216f1958fc599a7c4c3d8c25c56
adelina gutu <w1705972@gmail.com>
2025-01-29 23:55:12 +0000

    controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,11 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+
+  # Only allow a list of trusted parameters through.
+  def garden_params
+  params.require(:garden).permit(:name, :banner_url)
+  end
+
 end
