e7004238ddc244d60bd249c7f85b953990d69c7d
adelina gutu <w1705972@gmail.com>
2025-01-30 23:29:58 +0000

    navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :user_clubs
+  has_many :clubs, through: :user_clubs
 end
